2f98fd8647fafe87dc524269405d2e3bc8d6a480
hemanthreddyjonnala <31050644+hemanthreddyjonnala@users.noreply.github.com>
2023-08-06 11:24:37 -0400

    Create sorting.py

---
+++ sorting.py
@@ -0,0 +1,14 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]: 
+        """ Insertion Sort """       
+        for i in range(1, len(nums)):
+            a = nums[i]
+            j = i-1
+            while j>=0 and nums[j]>a:
+                # loop will continue till the sub array got sorted.
+                # print(f"i:{i}, j:{j}, {nums[j]}>{a}, {nums}")
+                nums[j+1] = nums[j]
+                j -= 1
+            nums[j+1] = a
+        return nums
+                
