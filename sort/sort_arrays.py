19245abdbad5c7057fef1c1c2f9f66749abbe2fb
hemanthreddyjonnala <hemanthreddyjonnala@gmail.com>
2023-08-07 18:50:33 -0400

    insertion sort

--- sort/sort_arrays.py
+++ sort/sort_arrays.py
@@ -13,9 +13,22 @@ class Sort:
                 j -= 1
             nums[j+1] = a
         return nums
+    
+    def selection_sort(self, nums: List[int]) -> List[int]: 
+        """ Selection Sort"""       
+        n = len(nums)
+        for i in range(n):
+            swap_index = i
+            for j in range(i+1, n):
+                # identify the swap position and swap the values
+                if nums[j]<nums[swap_index]:
+                    swap_index = j
+            nums[swap_index], nums[i] = nums[i], nums[swap_index]
+        return nums
                 
 nums = [5,2,3,1]
 
 sort_object = Sort()
 
-print(sort_object.insertion_sort(nums))
+print(f"insertion sort: {sort_object.insertion_sort(nums)}")
+print(f"slection sort: {sort_object.selection_sort(nums)}")
